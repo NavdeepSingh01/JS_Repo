7c94d01fcc3218eae1cc1829b550ce4ab48a3a78
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2024-12-24 13:23:03 +0000

    chng 24-12

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,23 @@
-console.log("Hello")
+/*
+    Variables
+*/
+
+const accountId=14556
+let accountEmail="xyz@gmail.com"
+var accountPassword="Navdeep"
+accountCity="Raipur" // wil not give error
+let accountState
+
+// accountId=2 not allowed -> declared as const can not be modified
+accountEmail="new@gmail.com" //can be modified
+
+console.log(accountEmail) // to print one value
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//two ways to declare variable -> var & let
+
+/*
+    var kabhi use mat karo plss 
+    block scope ki aissi taisi
+*/
+
+//accountState will be -> Undefined

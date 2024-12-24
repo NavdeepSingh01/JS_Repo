234e16865a08a5042b496f831f39624e663b62d9
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2024-12-24 13:43:09 +0000

    newchanges

---
+++ 01_Basics/dtype.js
@@ -0,0 +1,32 @@
+/*
+    Learning About Datatype
+*/
+
+"use strict"; //new code standard will be used
+// alert(3+3) using node not browser
+
+console.log("hello");
+console.log("hi");
+
+let name="nvdp"
+let age=17
+let loggedIn= true
+let newUser= null
+
+/*
+ ***primitive**
+ string enclosed by double quotes
+ number upto 2^53
+ bigint
+ boolean ->true/false
+ null standalone value
+ undefined
+ symbols -> for uniqueness
+*/
+
+console.log(typeof name);
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+
+
+

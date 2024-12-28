c55951e1956f20088a1536f2ce413c2dbad80e9b
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2024-12-28 13:12:30 +0000

    conversion of dtypes

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,26 @@
+let score= "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnum=Number(score);
+console.log(typeof(valueinnum));
+console.log(valueinnum);
+
+
+//"33"->33
+//"33abc" -> NaN
+
+let l="nvdp";
+let converted=Boolean(l);
+console.log(converted);
+console.log(typeof converted);
+
+
+// 1->true
+// 0-> false
+//"nvdp" ->true
+
+let num=55;
+let stringnum=String(num);
+console.log(num);
+console.log(typeof num);

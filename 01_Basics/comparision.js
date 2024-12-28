f0fa4d459327f973f1e7e99d94fb3375023f00c0
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2024-12-28 13:47:38 +0000

    comparison of same or diff dtypes

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,15 @@
+// console.log(2>5);
+// console.log(2>=5);
+// console.log(2==5);
+// console.log(2<=5);
+// console.log(2!=5);
+
+
+// console.log("2">=2);->true
+// console.log("02">=2);->true
+// automatically converts to desired dtype
+
+// console.log(2=="2");->true only checks value
+// console.log(2==="2");->false checks both value and datatype
+
+//avoid conversion of null and undefined

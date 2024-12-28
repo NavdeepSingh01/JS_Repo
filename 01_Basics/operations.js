aac49e7c730ac0535f41d5eb09c7a78a37ad67af
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2024-12-28 13:33:03 +0000

    different type of operations of each dtype

---
+++ 01_Basics/operations.js
@@ -0,0 +1,46 @@
+//*****operations******//
+
+// let val=5;
+// let negval=-val;
+
+
+// operations on number
+
+// console.log(val);
+// console.log(negval);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+
+//operations on string
+
+// let str1="Hello";
+// let str2=" Navdeep";
+// let str3=str1+str2;
+// console.log(str3);
+
+
+// console.log("1"+6);
+// ->outputs 16 as string
+// console.log("1"+2+2);
+// ->outputs->122 as string
+// console.log(2+1+"3");
+// ->outpus 33 as string
+
+
+
+
+
+// console.log(true);->true
+// console.log(+true);->1
+// console.log(+"");->0
+
+// let gameCounter=100;
+// console.log(gameCounter);
+// console.log(++gameCounter);
+
+

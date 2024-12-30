d7641a4ce6361e6f26d47905f21372fe238d9fc9
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2024-12-30 07:11:54 +0000

    string basics

---
+++ 01_Basics/basicString.js
@@ -0,0 +1,38 @@
+const name="Nvdp";
+const repoCount=50;
+
+// console.log(name + repoCount + "value"); not a good method
+// console.log(`Hello my name is ${name} ans my repocount is ${repoCount}`); string interpolation better than previous one
+
+const newName=new String("Navdeep");
+// console.log(newName);
+// console.log(newName[0]); print char at 0th index
+// console.log(newName.length);prints length of string
+// console.log(newName.toUpperCase); -> return uppercase did not change the orignal string
+// console.log(newName.charAt(2));
+// console.log(newName.indexOf('H')); return -1 because didn,t exist
+// console.log(newName.indexOf('N')); present at 0th index
+
+const newStr=newName.substring(0,4);
+// console.log(newStr); substring function takes to values -> starting index and end index index end index is not included
+
+const newString="   Hello   ";
+// console.log(newString.trim()); trims start and ending spaces also exist function to trimstart and trimend
+
+
+
+
+
+const newStr2="Hello Navdeep You Are Awesome";
+// const chngStr=newStr2.replace('Navdeep','Nvdp')->replacing a certain word
+// console.log(chngStr);
+
+// console.log(newStr2.split(" ")); splitting the string base on a seperator
+
+////read mdn documentation
+
+
+
+
+
+

4335a09aa64e4b7e768142e322029c94f67dd347
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2025-01-01 07:31:39 +0000

    arr basics

---
+++ 02_Basics/arr.js
@@ -0,0 +1,43 @@
+const myArr=[5,7,9,"Hello",['c',"HandSome"]];// an array can have values of different dtype
+//array are resizable size can be changed
+const heroAr=new Array(1,2,4,'Hey');
+// console.log(heroAr[3]);
+
+//array methods
+// console.log(myArr.length);
+// myArr.push(9);
+// myArr.pop();
+// myArr.unshift(9); -> insert 9 at front shift all the elemt till last
+// myArr.shift(); -> deletes the first element shifting all the other
+
+// console.log(myArr.indexOf(7)); ->return the index of the element
+
+// const Arr=myArr.join();
+// // console.log(Arr);
+// console.log(typeof Arr);
+
+// const newArr=[1,2,3,4,5];
+// const arrA=newArr.slice(0,3);
+// console.log(arrA);
+// console.log(newArr);
+// const arrB=newArr.splice(0,3);
+// console.log(newArr);
+// console.log(arrB);
+
+
+/*
+    In this section we learnt about 
+    arr declaration 
+    properties 
+    push pop shift unshift 
+    slice splice 
+    look for more in mdn documentation 
+*/
+
+
+
+
+
+
+
+

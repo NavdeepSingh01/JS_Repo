7f90d652beae8078d1700465b0d5af8691bec57c
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2025-02-01 13:37:02 +0000

    fnctionsjs

---
+++ 03_Basics/01_functions.js
@@ -0,0 +1,18 @@
+// function sayMyName(){
+//     console.log("H");
+//     console.log("I");
+//     console.log("T");
+//     console.log("E");
+//     console.log("S");
+//     console.log("H");
+// }
+// console.log(sayMyName());
+function addTwoNumber(number1,number2){
+    return number1+number2
+}
+const result=addTwoNumber(3,5);
+console.log(result);
+function LoginUserName(username){
+    return `${username} just logged in`
+}
+console.log(LoginUserName("Navdeep"));

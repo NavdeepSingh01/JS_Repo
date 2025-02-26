3413198b3906ee4367e26129c9222d852b2c80ce
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2025-02-26 12:53:01 +0000

    final_

---
+++ 03_Basics/03_arrow.js
@@ -0,0 +1,33 @@
+// const user = {
+//     username: "hitesh",
+//     price: 999,
+
+//     welcomeMessage: function() {
+//         console.log(`${this.username} , welcome to website`);
+//         console.log(this);
+//     }
+
+// }
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// const chai =  () => {
+//     let username = "hitesh"
+//     console.log(this);// arrow function
+// }
+// chai()
+
+const addTwo = (num1,num2)=>{
+    return num1+num2
+} //arrow func
+
+// const addTwo = (num1, num2) =>  num1 + num2 //implicit return

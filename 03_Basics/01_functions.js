e2ae6902e4cea0c950f0ff9c7a2d0c8b2ca95360
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2025-02-26 07:54:06 +0000

    2nd chng

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -7,12 +7,29 @@
 //     console.log("H");
 // }
 //sayMyName();
-function addTwoNumber(number1,number2){
-    return number1+number2  
+// function addTwoNumber(number1,number2){
+//     return number1+number2  
+// }
+// const result=addTwoNumber(3,5);
+// console.log(result);
+// function LoginUserName(username){
+//     return `${username} just logged in`
+// }
+// console.log(LoginUserName("Navdeep"));
+
+
+// function calculateCartPrice(...num1){
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(200,400,500));
+
+const user = {
+    username: "hitesh",
+    prices: 199
 }
-const result=addTwoNumber(3,5);
-console.log(result);
-function LoginUserName(username){
-    return `${username} just logged in`
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.prices}`);
 }
-console.log(LoginUserName("Navdeep"));
+handleObject(user);

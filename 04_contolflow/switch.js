3f3a7226970e582aecf7be9bf8c168d5698d242c
Navdeep Singh Saluja <144531682+NavdeepSingh01@users.noreply.github.com>
2025-02-27 07:05:35 +0000

    cmt

---
+++ 04_contolflow/switch.js
@@ -0,0 +1,12 @@
+const month=2;
+switch(month){
+    case 1 : 
+        console.log("Jan")
+        break;
+    case 2 :
+        console.log("Feb");
+        break;
+    default :
+        console.log("Other");
+        break;
+}
